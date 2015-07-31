3f1f517ef007fc37d2227e285958bd6208d9bad2
Mayur <panchal.mayur2@gmail.com>
2015-08-01 04:49:53 +1200

    Added some stuff to do with forms

---
+++ Flatline/app/src/main/java/nz/flatline/flatline/FlatSetupActivity.java
@@ -0,0 +1,16 @@
+package nz.flatline.flatline;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mayur on 1/08/2015.
+ */
+public class FlatSetupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flat_setup);
+    }
+}
