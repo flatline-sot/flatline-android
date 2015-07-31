e928885595993b5bbbadf9dcb703e7ef9a35707b
Mayur <panchal.mayur2@gmail.com>
2015-08-01 10:30:37 +1200

    Worked on the forms, and also cleaned up my resource referencing

--- Flatline/app/src/main/java/nz/flatline/flatline/FlatSetupActivity.java
+++ Flatline/app/src/main/java/nz/flatline/flatline/FlatSetupActivity.java
@@ -2,15 +2,28 @@ package nz.flatline.flatline;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Mayur on 1/08/2015.
  */
 public class FlatSetupActivity extends Activity {
 
+    Button submitButton, backButton;
+    TextView actionBarText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flat_setup);
+        initializeViews();
+        actionBarText.setText(R.string.flat_setup_title);
+    }
+
+    private void initializeViews() {
+        submitButton = (Button) findViewById(R.id.submit_button);
+        backButton = (Button) findViewById(R.id.back_button);
+        actionBarText = (TextView) findViewById(R.id.action_bar_title);
     }
 }
