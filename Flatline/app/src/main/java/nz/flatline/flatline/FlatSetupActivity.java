8a2ed6cd2d345dc5057fdf86e5e8ecd1c2150208
Mayur <panchal.mayur2@gmail.com>
2015-08-01 23:04:45 +1200

    Setup page done, and flow is kinda working

--- Flatline/app/src/main/java/nz/flatline/flatline/FlatSetupActivity.java
+++ Flatline/app/src/main/java/nz/flatline/flatline/FlatSetupActivity.java
@@ -1,21 +1,26 @@
 package nz.flatline.flatline;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Mayur on 1/08/2015.
  */
-public class FlatSetupActivity extends Activity {
+public class FlatSetupActivity extends Activity implements View.OnClickListener{
 
-    Button submitButton, backButton;
-    TextView actionBarText;
+    Button submitButton;
+    EditText name, flat_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,20 @@ public class FlatSetupActivity extends Activity {
     }
 
     private void initializeViews() {
+        name = (EditText) findViewById(R.id.name);
+        flat_name = (EditText) findViewById(R.id.flat_name);
         submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(TextUtils.isEmpty(name.getText())) {
+            Toast.makeText(FlatSetupActivity.this, "Please enter a name!", Toast.LENGTH_SHORT).show();
+        } else if(TextUtils.isEmpty(flat_name.getText())) {
+            Toast.makeText(FlatSetupActivity.this, "Please enter a flat name!", Toast.LENGTH_SHORT).show();
+        } else {
+            startActivity(new Intent(FlatSetupActivity.this, HomepageActivity.class));
+        }
     }
 }
